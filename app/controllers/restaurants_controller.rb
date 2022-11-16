3e7f622909bdd8aec0b95526bb36c44ec4bfca9d
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-11-16 18:01:09 +0100

    2models

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+    def create
+    @restaurants = Restaurants
+
+    end
+    def show
+
+
+    end
 end
