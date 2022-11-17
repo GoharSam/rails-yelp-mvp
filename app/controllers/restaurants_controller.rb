f5bfcae21bea6b1344dc1cde82fcaff30cec4c6c
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-11-17 22:39:05 +0100

    yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,15 +4,27 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    @restaurants = Restaurant.new
+    @restaurant = Restaurant.new
   end
 
-    def create
-    @restaurants = Restaurants
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
     end
-    def show
+  end
 
+  private
 
-    end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,18 @@
 class ReviewsController < ApplicationController
-def new
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_entity
+    end
+  end
 
-end
-def create
+  private
 
-end
-
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
